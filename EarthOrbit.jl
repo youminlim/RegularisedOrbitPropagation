ba3a231d5a9da8172f8af84c8de64783b62e3f88
youminlim <lim.ym5327@gmail.com>
2022-11-27 17:45:46 +0000

    deleted

--- EarthOrbit.jl
+++
@@ -1,7 +0,0 @@
-# Module to store global constants for Earth Orbits
-
-module Earth
-    G = 6.67e-11    # Gravitaional Constant
-    M = 5.972*10^24 # Mass of Earth - kg
-    R = 6379        # Radius of Earth - Km
-end 
