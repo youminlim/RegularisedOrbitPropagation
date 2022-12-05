bdae48d6d5b9f8767dfde24db74f00148c057879
youminlim <lim.ym5327@gmail.com>
2022-12-05 23:14:02 +0000

    fixed calculation of drag acceleration, changed input parameters

--- DragPerturbation.jl
+++ DragPerturbation.jl
@@ -1,10 +1,12 @@
-function drag(BC, ρ, v̲)
+function drag(BC, u)
     """
     Calculates the perturbing acceleration from a drag force
     """
-
-    v̲ᵣ = v̲ - v̲ₐ
-    vᵣ = norm(v̲ᵣ)
     
+    r̲ = u[1:3]
+    ρ = atmosphere(norm(r̲) - 6371)
+    v̲ᵣ = u[7:9]
+    vᵣ = norm(v̲ᵣ)
+
     return -(1/2) * ρ * vᵣ * BC * v̲ᵣ
 end
