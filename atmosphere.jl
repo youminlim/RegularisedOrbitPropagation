d9f4ab1b948aaeb1bf8f575a905b06ba968fea44
youminlim <lim.ym5327@gmail.com>
2022-12-05 21:55:20 +0000

    atmosphere model for earth. Incomplete, but will use a simple exponential model.

---
+++ atmosphere.jl
@@ -0,0 +1,11 @@
+function atmosphereEarth(altitude)
+    """
+    Calculates the air density for a given altitude above Earth sea level
+    Inputs:
+        altitude - altitude above Earth sea level
+    Outputs:
+        ρ - density at specified altitude above Earth sea level
+    """
+
+    
+end
