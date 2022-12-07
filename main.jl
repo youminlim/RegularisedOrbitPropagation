b873ff21fa0de8339d62ef43ff6bd20a19bcf143
youminlim <lim.ym5327@gmail.com>
2022-12-07 19:13:31 +0000

    added some new functions to help with repeated processes, changed plotting package from Plots to GLMakie for interactivity in 3D trajectory plot

--- main.jl
+++ main.jl
@@ -1,4 +1,4 @@
-using Base: MainInclude
+using Base: MainInclude, ForwardOrdering
 """ Numerical Propagation for 2 Body Problem with Cowell's Method """
 
 # Add Packages and Libraries
@@ -14,7 +14,6 @@ include("atmosphere.jl")
 
 using LinearAlgebra
 using DifferentialEquations
-using Plots
 using GeometryBasics
 using GLMakie
 
@@ -123,6 +122,31 @@ function f!(du, u, p, t)
 
 end
 
+# Extract state vectors from solution of solve() to convert to COEs
+function state2coe(solution, μ)
+    solutionCOE = []
+
+    for i in solution.u
+        push!(solutionCOE, coe(i, μ))
+    end
+
+    return solutionCOE
+end
+
+# Extract each COE
+function coeExtractor(solutionCOE)
+    angularMomentum = getindex.(solutionCOE, 1)
+    inclination = getindex.(solutionCOE, 2)
+    raan = getindex.(solutionCOE, 3)
+    eccentricity = getindex.(solutionCOE, 4)
+    aop = getindex.(solutionCOE, 5)
+    trueAnomaly = getindex.(solutionCOE, 6)
+
+    return [angularMomentum, inclination, raan, eccentricity, aop, trueAnomaly]
+end
+
+
+
 # Define global constants
 G = 6.67e-20
 centralBody = Earth()
@@ -136,37 +160,28 @@ tspan = (0.0, 160000.0)
 # solve keplerian Trajectory
 perturbations = defaultPerturbations()
 p = defaultParameters()
-p["perturbations"]["J2"] = true
-p["perturbations"]["Drag"] = true
 input = parameters(p)
 
 problem = ODEProblem(f!, u̲₀, tspan, input)
-solution = solve(problem, RK4(), dt = 1)
+solution = solve(problem, RK4())
+
+# Solve perturbation trajectory
+p["perturbations"]["J2"] = true
+p["perturbations"]["Drag"] = true
+inputPerturbed = parameters(p)
+
+problemPerturbed = ODEProblem(f!, u̲₀, tspan, inputPerturbed)
+solutionPerturbed = solve(problemPerturbed, RK4())
 
 # Convert state vector data to COEs
 begin
-    solutionCOE = []
-    for i in solution.u
-        push!(solutionCOE, coe(i, μ))
-    end
-    angularMomentum = getindex.(solutionCOE, 1)
-    #angularMomentum = angularMomentum/maximum(angularMomentum)
-    inclination = getindex.(solutionCOE, 2)
-    #inclination = inclination/maximum(inclination)
-    raan = getindex.(solutionCOE, 3)
-    #raan = raan/maximum(raan)
-    eccentricity = getindex.(solutionCOE, 4)
-    #eccentricity = eccentricity/maximum(eccentricity)
-    aop = getindex.(solutionCOE, 5)
-    #aop = aop/maximum(aop)
-    trueAnomaly = getindex.(solutionCOE, 6)
-    #trueAnomaly = trueAnomaly/maximum(trueAnomaly)
+    coes = coeExtractor(state2coe(solution, μ))
+    coesPerturbed = coeExtractor(state2coe(solutionPerturbed, μ))
 
     time = solution.t / 3600 
+    timePerturbed = solutionPerturbed.t / 3600
 end
 #
-
-error = maximum(angularMomentum) - minimum(angularMomentum)
 
 # Plot data
 begin
@@ -181,42 +196,117 @@ begin
     y = solution[2,:]
     z = solution[3,:]
 
-    lines!(ax, x, y, z, label = "Satellite", color = "red")
+    xₚ = solutionPerturbed[1,:]
+    yₚ = solutionPerturbed[2,:]
+    zₚ = solutionPerturbed[3,:]
+
+    lines!(ax, x, y, z, label = "Keplerian Trajectory", color = "red")
+    lines!(ax, xₚ, yₚ, zₚ, label = "Perturbed Trajectory", color = "green")
     mesh!(ax, Sphere(Point3f0(0), 6371))
 
-    trajectoryPlot = plot(solution, idxs = (1, 2, 3), title = "Satellite Trajectory", xlabel = "x", ylabel = "y", zlabel = "z", label = "Keplerian", size = (1000, 500))
+    #trajectoryPlot = plot(solution, idxs = (1, 2, 3), title = "Satellite Trajectory", xlabel = "x", ylabel = "y", zlabel = "z", label = "Keplerian trajectory", size = (1000, 500))
+    #plot!(trajectoryPlot, solutionPerturbed, idxs = (1, 2, 3), label = "Pertubed trajectory")
     fig[1, 2] = Legend(fig, ax, "Legend")
     fig
 end
 
-begin
+# Plot COEs
+function CoePlotter(COEs, time, additionalCOEs::Vector{Vector{Float64}})
     # Plot COEs
-    angularMomentumPlot = plot(time, angularMomentum, title = "Angular Momentum")
-    inclinationPlot = plot(time, inclination, title = "Inclination")
-    raanPlot = plot(time, raan, title = "RAAN Ω")
-    eccentricityPlot = plot(time, eccentricity, title = "Eccentricity")
-    aopPlot = plot(time, aop, title = "Argument of Perigee ω")
-    trueAnomalyPlot = plot(time, trueAnomaly, title = "True Anomaly θ")
-end
-    
+    angularMomentumPlot = plot(time, COEs[1], title = "Angular Momentum")
+    inclinationPlot = plot(time, COEs[2], title = "Inclination")
+    raanPlot = plot(time, COEs[3], title = "RAAN Ω")
+    eccentricityPlot = plot(time, COEs[4], title = "Eccentricity")
+    aopPlot = plot(time, COEs[5], title = "Argument of Perigee ω")
+    trueAnomalyPlot = plot(time, COEs[6], title = "True Anomaly θ")
 
-#
+    # Plot names 
+    plotNames = [angularMomentumPlot, inclinationPlot, raanPlot, eccentricityPlot, aopPlot, trueAnomalyPlot]
 
-# Set Perturbations
-# perturbations["J2"] = true
+    # Plot perturbed COEs
+    for i in range(1, length(plotNames))
+        plot!(plotNames[i], time, additionalCOEs[i])
+    end
 
-# # Define parameter
-# p["centralBody"] = Earth()
-# p["perturbations"] = perturbations
-# inputPerturbed  = parameters(p)
-
-# problemPerturbed = ODEProblem(f!, u̲₀, tspan, inputPerturbed)
-# solutionPerturbed = solve(problemPerturbed, reltol=1e-9, saveat = 0.1)
-
-# # Plot data
-#     plot!(trajectoryPlot, solutionPerturbed, idxs = (1, 2, 3), label = "J2 Perturbation")
-# #
-    
-# Display Plots
-    display(trajectoryPlot)
     display(plot(angularMomentumPlot, inclinationPlot, raanPlot, eccentricityPlot, aopPlot, trueAnomalyPlot, layout = (3, 2), size = (1250, 1150)))
+end
+ 
+# Setup Plot
+begin
+    # Create Figure
+    fig = Figure(resolution = (1275, 1200))
+
+    # Create Grids
+        ga = fig[1:2, 1] = GridLayout()
+        # gb = fig[2, 1] = GridLayout()
+        gc = fig[1, 2] = GridLayout()
+        gd = fig[2, 2] = GridLayout()
+
+    # Labels
+    labels = ["Keplerian", "Perturbed"]
+
+    # For ga
+        # Create Axis
+        ax1 = Axis(ga[1, 1], title = "Angular Momentum", ylabel = "h in kgm²/s")
+        ax2 = Axis(ga[2, 1], title = "Inclination", ylabel = "i in °")
+        ax3 = Axis(ga[3, 1], title = "RAAN Ω", ylabel = "Ω in °")
+        ax4 = Axis(ga[4, 1], title = "Eccentricity", ylabel = "e, dimensionless")
+        ax5 = Axis(ga[5, 1], title = "AOP ω", ylabel = "ω in °")
+        ax6 = Axis(ga[6, 1], title = "True Anomaly θ", xlabel = "Hours", ylabel = "θ in °")
+        axis = [ax1, ax2, ax3, ax4, ax5, ax6]
+
+        # Fill axes with data
+        for i in range(1, length(axis))
+            lines!(axis[i], time, coes[i], label = labels[1], color = "red")
+            lines!(axis[i], timePerturbed, coesPerturbed[i], label = labels[2], color = "green")
+        end
+        for i in axis[1:5]
+            hidexdecorations!(i, grid = false)
+        end
+    #
+    # For gc
+        # Create Axis
+        axc = Axis3(gc[1, 1], title = "Satellite Trajectory")
+
+        # Prepare data
+        x = solution[1,:]
+        y = solution[2,:]
+        z = solution[3,:]
+
+        xₚ = solutionPerturbed[1,:]
+        yₚ = solutionPerturbed[2,:]
+        zₚ = solutionPerturbed[3,:]
+
+        # Fill axes with data
+        lines!(axc, x, y, z, label = "Keplerian Trajectory", color = "red")
+        lines!(axc, xₚ, yₚ, zₚ, label = "Perturbed Trajectory", color = "green")
+        mesh!(axc, Sphere(Point3f0(0), 6371))
+    #
+    # For gd 
+        # Create Axis
+        ax = Axis(gd[1, 1], title = "x distance")
+        ay = Axis(gd[2, 1], title = "y distance")
+        az = Axis(gd[3, 1], title = "z distance")
+        axisDistance = [ax, ay, az]
+
+        # Prepare data
+        keplerian = [x, y, z]
+        perturbed = [xₚ, yₚ, zₚ]
+
+        # Fill axes with data
+        for i in range(1, length(axisDistance))
+            lines!(axisDistance[i], time, keplerian[i], color = "red")
+            lines!(axisDistance[i], timePerturbed, perturbed[i], color = "green")
+        end
+        for i in axisDistance[1:2]
+            hidexdecorations!(i, grid = false)
+        end
+    #
+
+    # leg = Legend(gd[1, 1], ax1)
+    # leg.tellheight = true
+
+    colsize!(fig.layout, 1, Auto(0.6))
+    colsize!(gc, 1, Auto())
+    fig
+end
