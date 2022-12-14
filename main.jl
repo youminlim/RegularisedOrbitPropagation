0dadc4d89c4cff9139c0fa3df45f1f0e8ac821d7
youminlim <lim.ym5327@gmail.com>
2022-12-14 19:30:45 +0000

    changed the solver variable names in order to reflect the changes made in the previous commit

--- main.jl
+++ main.jl
@@ -66,63 +66,6 @@ function TwoBodyInitial(u)
     return [u[1:3]; 0; 0; 0; u[4:6]; 0; 0; 0] 
 end
 
-# 2 Body problem with Cowell's Method for a satellite orbit about a central body
-function f!(du, u, p, t)
-    """
-    Inputs:
-        state vector u - 6 states, [x₁, y₁, z₁, x₂, y₂, z₂, ẋ₁, ẏ₁, ż₁, ẋ₂, ẏ₂, ż₂], as motion of central body is not concerned
-        p - parameters to pass to function, has structure : [satelliteMass, centralBody, perturbations]
-            satelliteMass - contains a value representing the mass of the satellite
-
-            centralBody - will have structure [mass, radius, J2] of the central body
-
-            perturbations - will have structure [nothing], override by adding in the string name of each perturbation type, i.e. "J2" in order to apply the accelerations from J2 to the system
-    Output:
-        du - the derivative of the state vector i.e. u̇
-    """
-
-    # Housekeeping
-    satelliteMass = p[1]
-    centralBody = p[2:4]
-    perturbations = p[4:length(p)]
-
-    x, y, z = u[1:3]
-    r̲ = [x, y, z]
-    r = norm(r̲)
-    du[1:6] = u[7:12]
-
-    G = 6.67e-20
-    μ₁ = (G * centralBody[1])
-    μ₂ = (satelliteMass * G)
-
-    # Central Body
-
-    # Calculate accelerations
-    du[7:9] = - μ₁ * r̲ / r^3
-    du[10:12] = μ₂ * r̲ / r^3
-
-    # Add perturbation effects
-    if "J2" in perturbations
-        # Housekeeping
-        R = centralBody[2]
-        J₂ = centralBody[3]
-        ẋJ2 = (1 - (5*z^2)/r^2) * x
-        ẏJ2 = (1 - (5*z^2)/r^2) * y
-        żJ2 = (3 - (5*z^2)/r^2) * z
-        a_J2 = (-(3/2)*(μ*J₂*R^2) / (r^5)) * [ẋJ2, ẏJ2, żJ2]
-
-        du[7:9] += a_J2
-    end
-
-    if "Drag" in perturbations
-        BC = 1 / 82
-        a_Drag = drag(BC, u)
-
-        du[7:9] += a_Drag
-    end
-
-end
-
 # Extract state vectors from solution of solve() to convert to COEs
 function state2coe(solution, μ)
     solutionCOE = []
@@ -179,7 +122,7 @@ perturbations = defaultPerturbations()
 p = defaultParameters()
 input = parameters(p)
 
-problem = ODEProblem(f!, u̲₀, tspan, input)
+problem = ODEProblem(twoBodySolver!, u̲₀, tspan, input)
 solution = solve(problem, RK4(), adaptive = false, dt = 10)
 
 # Solve perturbation trajectory
@@ -187,7 +130,7 @@ p["perturbations"]["J2"] = false
 p["perturbations"]["Drag"] = true
 inputPerturbed = parameters(p)
 
-problemPerturbed = ODEProblem(f!, u̲₀, tspan, inputPerturbed)
+problemPerturbed = ODEProblem(twoBodySolver!, u̲₀, tspan, inputPerturbed)
 solutionPerturbed = solve(problemPerturbed, RK4(), adaptive = false, dt = 10)
 
 # Convert state vector data to COEs
